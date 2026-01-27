9ecc3aa441497109c8d5a89e863c60be0861eb68
galang225 <galangaditya334@gmail.com>
2026-01-27 21:50:43 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    subdomain: {
+        "langz4youu.my.id": {
+            "zone": "785378612cbe0659ce27f9653b60e52a",
+            "apitoken": "NGSVHsKLIeRw3qdi8XhQO_-Z_CpVAtcYMalp7ZSu"
+        },
+        "galanghostxoline.my.id": {
+            "zone": "6ac7dc4e93652bfa6bb1c79d2d2d49e7",
+            "apitoken": "M64KlwR_p7wxc15UkzD0x9k1VgdR7O86ADThGt54"
+        }
+    }
+};
